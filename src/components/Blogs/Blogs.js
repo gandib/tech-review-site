d92bc591f6f96eab3ce998e699b02fa1aac0cb1e
Gandib Roy <gandibroy11@gmail.com>
2022-07-31 19:25:57 +0600

    Blogs page's question and answer are added

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -4,7 +4,15 @@ import './Blogs.css';
 const Blogs = () => {
     return (
         <div>
+            <h2>Question and Answer</h2>
+            <div className='container'>
+                <h4>What is the purpose of Context API?</h4>
+                <p>Answer: React context is an essential tool for every React developer to know. React context allows us to pass down and use data in whatever component we need in our React app without using props. In other words, React context allows us to share data across our components more easily. React context is great when you are passing data that can be used in any component in your application. Data should be placed on React context that does not need to be updated often.</p>
 
+                <h4>What is Symantic Tag?</h4>
+                <p>Answer: Semantic tags clearly define the purpose of the HTML element. It also describes the type of content that the element should contain. For example, header, footer, article are considered semantic elements as they clearly describe their purpose and the type of content they should enclose.
+                    New HTML5 semantic tags to structure the document in a meaningful way are: header, nav, article, section, aside, footer. Semantic tags also help search engines to parse the webpage correctly and find accurate information. Thus, helping search engines to correctly index the page. </p>
+            </div>
         </div>
     );
 };
